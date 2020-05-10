60476344ede1928cb946e662a803b3a33d6fccf7
qabRieL99 <mehmetaliumac@gmail.com>
2020-05-10 16:15:52 +0300

    Add project files.

---
+++ toolTerraria.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Darari
+{
+    partial class toolTerraria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1343, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Image = global::Darari.Properties.Resources.folder__plus;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Image = global::Darari.Properties.Resources.disk;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "JSON Files|*.json";
+            this.openFileDialog1.Title = "Please select a .json file";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 602);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1343, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 16);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 16);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(12, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 36);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.textBox2.Location = new System.Drawing.Point(12, 120);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1068, 201);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(937, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Visible = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Trebuchet MS", 17F);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 327);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1068, 201);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.Enabled = false;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(1086, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(257, 574);
+            this.listBox1.TabIndex = 6;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.trackBar1.Location = new System.Drawing.Point(0, 546);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(1086, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Image = global::Darari.Properties.Resources.arrow_return_270;
+            this.button4.Location = new System.Drawing.Point(599, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(41, 36);
+            this.button4.TabIndex = 32;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Image = global::Darari.Properties.Resources.tick_octagon;
+            this.button3.Location = new System.Drawing.Point(552, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 36);
+            this.button3.TabIndex = 31;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Image = global::Darari.Properties.Resources.arrow_180;
+            this.button1.Location = new System.Drawing.Point(458, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 36);
+            this.button1.TabIndex = 29;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Image = global::Darari.Properties.Resources.arrow;
+            this.button2.Location = new System.Drawing.Point(505, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 36);
+            this.button2.TabIndex = 30;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolTerraria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1343, 624);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "toolTerraria";
+            this.Text = "Terraria Text Editor v1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ toolTerraria.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace Darari
+{
+    public partial class toolTerraria : Form
+    {
+        public toolTerraria()
+        {
+            InitializeComponent();
+        }
+
+
+
+        bool degisken;
+
+        void Yukle()
+        {
+            listBox1.Items.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            richTextBox1.Clear();
+            trackBar1.Value = 0;
+
+            textBox3.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+            foreach (string item in textBox3.Text.Split('\n'))
+            {
+                if (item != "")
+                {
+                    listBox1.Items.Add(item.ToString().Trim());
+                }
+            }
+            listBox1.SetSelected(0, true);
+            toolStripStatusLabel1.Text = listBox1.SelectedIndex.ToString() + " / " + (listBox1.Items.Count - 1).ToString() + "   %" + Math.Round((decimal)(listBox1.SelectedIndex * 100) / (listBox1.Items.Count - 1), 1);
+            int max = listBox1.Items.Count - 1;
+            trackBar1.Maximum = max;
+        }
+
+        void Ayristir()
+        {
+            if (listBox1.SelectedItem.ToString() == "{" || listBox1.SelectedItem.ToString() == "}," || listBox1.SelectedItem.ToString() == "}" || listBox1.SelectedItem.ToString().Substring(listBox1.SelectedItem.ToString().Length - 1) == "{")
+            {
+                toolStripStatusLabel1.Text = listBox1.SelectedIndex.ToString() + " / " + (listBox1.Items.Count - 1).ToString() + "   %" + Math.Round((decimal)(listBox1.SelectedIndex * 100) / (listBox1.Items.Count - 1), 1);
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                textBox2.Text = "***************";
+                // textBox2.Enabled = false;
+                button3.Enabled = false;
+                richTextBox1.Enabled = false;
+                richTextBox1.Clear();
+            }
+            else
+            {
+                if (listBox1.SelectedItem.ToString().Substring(listBox1.SelectedItem.ToString().Length - 1) == ",")
+                {
+                    degisken = true;
+                    textBox2.Clear();
+
+
+
+                    if (richTextBox1.Text.Length == 0)
+                    {
+                        button3.Enabled = false;
+                    }
+                    else
+                    {
+                        button3.Enabled = true;
+                    }
+
+
+
+
+                    button4.Enabled = true;
+                    richTextBox1.Enabled = true;
+                    String[] parcalar = listBox1.SelectedItem.ToString().Split(new string[] { ":" }, 2, StringSplitOptions.None);
+                    textBox1.Text = parcalar[0];
+                    textBox2.Text = parcalar[1].Remove(0, 2).Remove(parcalar[1].Length - 4, 2);
+                    toolStripStatusLabel1.Text = listBox1.SelectedIndex.ToString() + " / " + (listBox1.Items.Count - 1).ToString() + "   %" + Math.Round((decimal)(listBox1.SelectedIndex * 100) / (listBox1.Items.Count - 1), 1);
+                    trackBar1.Value = listBox1.SelectedIndex;
+                    richTextBox1.Clear();
+                }
+                else
+                {
+                    degisken = false;
+                    textBox2.Clear();
+
+                    if (richTextBox1.Text.Length ==0)
+                    {
+                        button3.Enabled = false;    
+                    }
+                    else
+                    {
+                        button3.Enabled = true;
+                    }
+                    
+                    
+                    button4.Enabled = true;
+                    richTextBox1.Enabled = true;
+                    String[] parcalar = listBox1.SelectedItem.ToString().Split(new string[] { ":" }, 2, StringSplitOptions.None);
+                    textBox1.Text = parcalar[0];
+                    textBox2.Text = parcalar[1].Remove(0, 2).Remove(parcalar[1].Length - 3, 1);
+                    toolStripStatusLabel1.Text = listBox1.SelectedIndex.ToString() + " / " + (listBox1.Items.Count - 1).ToString() + "   %" + Math.Round((decimal)(listBox1.SelectedIndex * 100) / (listBox1.Items.Count - 1), 1);
+                    trackBar1.Value = listBox1.SelectedIndex;
+                    richTextBox1.Clear();
+                }
+            }
+        }
+
+
+        void Kaydet()
+        {
+            if (degisken)
+            {
+                int satır = listBox1.SelectedIndex;
+                string num = textBox1.Text + ": " + "\"" + richTextBox1.Text + "\",";
+                listBox1.Items[satır] = num;
+                richTextBox1.Clear();
+            }
+            else
+            {
+                int satır = listBox1.SelectedIndex;
+                string num = textBox1.Text + ": " + "\"" + richTextBox1.Text + "\"";
+                listBox1.Items[satır] = num;
+                richTextBox1.Clear();
+            }
+            listBox1.SelectedIndex++;
+        }
+
+        void DosyaKaydet()
+        {
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(openFileDialog1.FileName);
+            foreach (var item in listBox1.Items)
+            {
+                SaveFile.WriteLine(item.ToString());
+            }
+
+            MessageBox.Show("Saved!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            SaveFile.Close();
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = trackBar1.Value;
+            Ayristir();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Yukle();
+                toolStripStatusLabel2.Text = openFileDialog1.SafeFileName;
+                Ayristir();
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DosyaKaydet();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex <= 0)
+            {
+
+            }
+            else
+            {
+                listBox1.SelectedIndex--;
+            }
+            Ayristir();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= listBox1.Items.Count - 1)
+            {
+
+            }
+            else
+            {
+                listBox1.SelectedIndex++;
+            }
+            Ayristir();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Kaydet();
+            Ayristir();
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                Kaydet();
+                Ayristir();
+                e.Handled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += textBox2.Text;
+            richTextBox1.Focus();
+            richTextBox1.Select(richTextBox1.Text.Length, 0);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length == 0)
+            {
+                button3.Enabled = false;
+            }
+            else
+            {
+                button3.Enabled = true;
+            }
+          
+        }
+    }
+}
+
+
